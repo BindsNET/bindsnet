6a985fc1bf93de4368ae10cfef05b0f574fd5c77
Dan Saunders <djsaunde@umass.edu>
2018-02-26 10:17:19 -0500

    Adding Bernoulli spike train encoding; updating Jupyter notebook demo accordingly.

--- bindsnet/encoding/__init__.py
+++ bindsnet/encoding/__init__.py
@@ -10,7 +10,7 @@ def get_poisson(data, time):
     
     Inputs:
         data (torch.Tensor or torch.cuda.Tensor): Tensor of shape [n_samples, n_1,
-            ..., n_k], with arbitrarily dimensionality [n_1, ..., n_k].
+            ..., n_k], with arbitrarily sample dimensionality [n_1, ..., n_k].
         time (int): Length of Poisson spike train per input variable.
     
     Yields:
@@ -18,7 +18,6 @@ def get_poisson(data, time):
             Poisson-distributed spikes parameterized by the data values.
 	'''
 	n_samples = data.size(0)  # Number of samples
-	
 	data = np.copy(data)
 	
 	for i in range(n_samples):
@@ -31,7 +30,7 @@ def get_poisson(data, time):
 		# proportional to spike inter-arrival time).
 		datum[datum != 0] = 1 / datum[datum != 0]
 
-		# Make the spike data.
+		# Make spike data from Poisson sampling.
 		s_times = np.random.poisson(datum, [time, size])
 		s_times = np.cumsum(s_times, axis=0)
 		s_times[s_times >= time] = 0
@@ -46,3 +45,38 @@ def get_poisson(data, time):
 
 		# Yield Poisson-distributed spike trains.
 		yield torch.Tensor(s)
+
+
+def get_bernoulli(data, time, max_prob=1.0):
+	'''
+	Generates Bernoulli-distributed spike trains based on input intensity. Inputs must
+	be non-negative. Spikes correspond to successful Bernoulli trials, with success
+	probability equal to (normalized in [0, 1]) input value.
+
+	Inputs:
+		data (torch.Tensor or torch.cuda.Tensor): Tensor of shape [n_samples,
+			n_1, ..., n_k], with arbitrary sample dimensionality [n_1, ..., n_k].
+		time (int): Length of Bernoulli spike train per input variable.
+		max_prob (float): Maximum probability of spike per Bernoulli trial.
+	'''
+	n_samples = data.size(0)  # Number of samples
+	data = np.copy(data)
+
+	for i in range(n_samples):
+		# Get i-th datum.
+		datum = data[i]
+		shape, size = datum.shape, datum.size
+		datum = datum.ravel()
+
+		# Normalize inputs and rescale (spike probability
+		# proportional to normalized intensity).
+		datum /= datum.max()
+		datum *= max_prob
+
+		# Make spike data from Bernoulli sampling.
+		s = np.random.binomial(1, datum, [time, size])
+		s = s.reshape([time, *shape])
+
+		# Yield Bernoulli-distributed spike trains.
+		yield torch.Tensor(s)
+   
