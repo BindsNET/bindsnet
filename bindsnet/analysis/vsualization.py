79eeaa6c61fdd6a5733d54e0318117637154cf80
hqkhan <hqkhan@umass.edu>
2018-04-06 02:16:44 -0400

    animation

--- bindsnet/analysis/vsualization.py
+++ bindsnet/analysis/vsualization.py
@@ -1,14 +1,12 @@
 import sys
-import torch
 import numpy as np
 
-from plotting import plot_input
 import matplotlib.pyplot as plt
 
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
+import matplotlib.animation as animation
 import matplotlib.gridspec as gridspec
-
 
 def spike_trains_main(data=None, n_ex=None, top_k=None, indices=None):
 #	fig = plt.figure(figsize=(10, 8))
@@ -24,10 +22,32 @@ def spike_trains_main(data=None, n_ex=None, top_k=None, indices=None):
 #		ax = plt.Subplot(fig, inner[j])
 		plot_spike_trains_examples(data, n_ex=n_ex)
 	
+ani = 0
+def create_movie(weights):
+	fig = plt.figure()
+	im = plt.imshow(weights[:, :, 0], cmap='hot_r', animated=True, vmin=0, vmax=1)
+	plt.axis('off'); plt.colorbar(im)
+		
+	def update(j):
+		im.set_data(weights[:, :, j])
+		return [im]
+	
+	global ani
+	ani = animation.FuncAnimation(fig, update, frames=weights.shape[-1], interval=1000, blit=True)
+	plt.show()
+		
+	
+def plot_weights_movie(weights):
+	n_examples = weights.shape[0]
+	ws = []
+	for i in range(n_examples):
+		ws.append(get_weights_for_example(weights[i]))
+	ws = np.concatenate(ws, axis=2)
+	create_movie(ws)
 	
 	
-def weights_for_example(weights=None, subsample=10):
-	pass
+def get_weights_for_example(w, sample_every=1):
+	return w[:, :, range(0, w.shape[2], sample_every)]
 
 def plot_spike_trains_for_example(spikes=None, n_ex=None, top_k=None, indices=None):
 	'''
@@ -39,15 +59,17 @@ def plot_spike_trains_for_example(spikes=None, n_ex=None, top_k=None, indices=No
 								Meant to replace top_k. 
 								
 	If both top_k and indices are left as default values (None), plot will include
-	all neurons
+	all neurons.
 	
 	'''
-	
-	
-	if top_k is None:
+
+	# Check that either both are None or only one of the parameters is
+	if top_k is None and indices is None:
+		top_k = spikes.shape[0]
+	elif top_k is None:
 		assert (indices is not None)
-	else:
-		assert (indices is None)
+	elif indices is None:
+		assert (top_k is not None)
 	
 	
 	# Figure out top_k locations
@@ -59,9 +81,15 @@ def plot_spike_trains_for_example(spikes=None, n_ex=None, top_k=None, indices=No
 	plt.eventplot(spike_per_neuron)
 	plt.show()
 	
+
 def main():
-	data = np.random.binomial(1, 0.25, size=[5, 20, 100])
-	plot_spike_trains_examples(data, 1, 5)
+	#data = np.random.binomial(1, 0.25, size=[5, 20, 100])
+	#plot_spike_trains_examples(data, 1, 5)
+	np.random.seed(0)
+	weights = np.random.random(size=(5, 4, 4, 20))
+	plot_weights_movie(weights)
+	
+	
 if __name__ == '__main__':
 	main()
 		
