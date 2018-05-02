411b479d94549ad0e3228369484d9602c8d47acc
djsaunde <djsaunde@umass.edu>
2018-05-02 14:11:23 -0400

    More encoding tests.

--- test/encoding/test_encoding.py
+++ test/encoding/test_encoding.py
@@ -1,6 +1,7 @@
 from bindsnet.encoding import *
 
 import torch
+import numpy as np
 
 class TestEncodings:
 	'''
@@ -19,6 +20,20 @@ class TestEncodings:
 
 					print('No. nodes: %d, no. timesteps: %d, max. firing prob.: %.2f, activity ratio: %.4f' % \
 						  (n, t, m, (datum.sum() * t) / spikes.float().sum()))
+	
+	def test_multidim_bernoulli(self):
+		print(); print('*** test_multidim_bernoulli ***'); print()
+		
+		for shape in [[5, 5], [10, 10], [25, 25]]:  # shape of nodes in layer
+			for t in [1, 10, 100]:  # number of timesteps
+				for m in [0.01, 0.1, 1.0]:  # maximum spiking probability
+					datum = torch.empty(shape).uniform_(0, m)
+					spikes = bernoulli(datum, time=t, max_prob=m)
+					
+					assert spikes.size() == torch.Size((t, *shape))
+
+					print('No. nodes: %d, no. timesteps: %d, max. firing prob.: %.2f, activity ratio: %.4f' % \
+						  (np.prod(shape), t, m, (datum.sum() * t) / spikes.float().sum()))
 		
 	def test_bernoulli_loader(self):
 		print(); print('*** test_bernoulli_loader ***'); print()
@@ -34,7 +49,7 @@ class TestEncodings:
 							assert spikes.size() == torch.Size((t, n))
 
 							print('No. samples: %d, no. nodes: %d, no. timesteps: %d, max. firing prob.: %.2f, activity ratio: %.4f' % \
-							  (s, n, t, m, (data[i].sum() * t) / spikes.float().sum()))
+							      (s, n, t, m, (data[i].sum() * t) / spikes.float().sum()))
 
 	
 	def test_poisson(self):
@@ -42,10 +57,13 @@ class TestEncodings:
 		
 		for n in [1, 10, 100, 1000]:  # number of nodes in layer
 			for t in [1, 10, 100, 1000]:  # number of timesteps
-				datum = torch.empty(n).uniform_(0, 1)
+				datum = torch.empty(n).uniform_(0, 100)
 				spikes = poisson(datum, time=t)
 				
 				assert spikes.size() == torch.Size((t, n))
+				
+				print('No. nodes: %d, no. timesteps: %d, activity ratio: %.4f' % \
+					  (n, t, (datum.sum() * t) / spikes.float().sum()))
 		
 	def test_poisson_loader(self):
 		print(); print('*** test_poisson_loader ***'); print()
@@ -53,9 +71,11 @@ class TestEncodings:
 		for s in [1, 10, 100]:  # number of data samples
 			for n in [1, 10, 100]:  # number of nodes in layer
 				for t in [1, 10, 100]:  # number of timesteps
-					data = torch.empty(s, n).uniform_(0, 1)
+					data = torch.empty(s, n).uniform_(0, 100)
 					spike_loader = poisson_loader(data, time=t)
 					
-					for spikes in spike_loader:
+					for i, spikes in enumerate(spike_loader):
 						assert spikes.size() == torch.Size((t, n))
 					
+						print('No. nodes: %d, no. timesteps: %d, activity ratio: %.4f' % \
+							  (n, t, (data[i].sum() * t) / spikes.float().sum()))
