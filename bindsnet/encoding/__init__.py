bab5afeb14ddd186e75d09be75058934d270bb59
djsaunde <djsaunde@umass.edu>
2018-05-10 10:56:14 -0400

    Adding a sort of rank order encoding of data into spikes, and example MNIST script.

--- bindsnet/encoding/__init__.py
+++ bindsnet/encoding/__init__.py
@@ -53,7 +53,7 @@ def bernoulli_loader(data, time=None, **kwargs):
 	
 	Yields:
 	
-		| (:code:`torch.Tensor`): Tensor of shape :code:`[time, n_1, ..., n_k]` of Bernoulli-distributed spikes.
+		| (:code:`torch.Tensor`): Tensors of shape :code:`[n_1, ..., n_k]` of Bernoulli-distributed spikes.
 	'''
 	if 'max_prob' in kwargs:
 		max_prob = kwargs['max_prob']
@@ -91,8 +91,8 @@ def poisson(datum, time, **kwargs):
 	
 	# Create spike trains from spike times.
 	s = np.zeros([time, size])
-	for idx in range(time):
-		s[s_times[idx], np.arange(size)] = 1
+	for i in range(time):
+		s[s_times[i], np.arange(size)] = 1
 
 	s[0, :] = 0
 	s = s.reshape([time, *shape])
@@ -110,7 +110,59 @@ def poisson_loader(data, time, **kwargs):
 
 	Yields:
 	
-		| (:code:`torch.Tensor`): Tensor of shape :code:`[time, n_1, ..., n_k]` of Poisson-distributed spikes.
+		| (:code:`torch.Tensor`): Tensors of shape :code:`[n_1, ..., n_k]` of Poisson-distributed spikes.
 	'''
 	for i in range(data.size(0)):
 		yield poisson(data[i], time)  # Encode datum as Poisson spike trains.
+
+
+def rank_order(datum, time, **kwargs):
+	'''
+	Encodes data via a rank order coding-like representation. One spike per neuron, temporally ordered by decreasing intensity. Inputs must be non-negative.
+	
+	Inputs:
+	
+		| :code:`data` (:code:`torch.Tensor`): Tensor of shape :code:`[n_samples, n_1, ..., n_k]`
+		| :code:`time` (:code:`int`): Maximum time of 
+
+	Returns:
+	
+		| (:code:`torch.Tensor`): Tensor of shape :code:`[time, n_1, ..., n_k]` of Poisson-distributed spikes.
+	'''
+	datum = np.copy(datum)
+	shape, size = datum.shape, datum.size
+	datum = datum.ravel()
+	
+	# Compute single spike times in order of decreasing intensity.
+	datum /= datum.max()
+	s_times = np.zeros(size)
+	s_times[datum != 0] = 1 / datum[datum != 0]
+	s_times *= time / s_times.max()
+	s_times = np.ceil(s_times).astype(np.int)
+	s_times[s_times > time] = time
+	
+	# Create spike times tensor.
+	s = np.zeros([time, size])
+	for i in range(size):
+		if s_times[i] != 0:
+			s[s_times[i] - 1, i] = 1
+	
+	s = s.reshape([time, *shape])
+	
+	return torch.Tensor(s).byte()
+
+def rank_order_loader(data, time, **kwargs):
+	'''
+	Lazily invokes :code:`bindsnet.encoding.rank_order` to iteratively encode a sequence of data.
+	
+	Inputs:
+	
+		| :code:`data` (:code:`torch.Tensor`): Tensor of shape :code:`[n_samples, n_1, ..., n_k]`
+		| :code:`time` (:code:`int`): Length of Poisson spike train per input variable.
+
+	Yields:
+	
+		| (:code:`torch.Tensor`): Tensors of shape :code:`[n_1, ..., n_k]` of rank order-encoded spikes.
+	'''
+	for i in range(data.size(0)):
+		yield rank_order(data[i], time)  # Encode datum as rank order-encoded spike trains.
