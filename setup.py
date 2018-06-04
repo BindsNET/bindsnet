3d4eb64799e79e49e9b2d1a8062ccbdea2072dd9
Dan Saunders <djsaunde@umass.edu>
2018-06-04 19:21:37 -0400

    PyPI is very hard!

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     long_description = f.read()
 
-version='0.1.2'
+version='0.1.3'
 
 setup(name='bindsnet',
       version=version,
@@ -14,7 +14,7 @@ setup(name='bindsnet',
       url='http://github.com/Hananel-Hazan/bindsnet',
       author='Daniel Saunders, Hananel Hazan, Darpan Sanghavi, Hassaan Khan',
       author_email='djsaunde@cs.umass.edu',
-      packages=['bindsnet'],
+      packages=find_packages(),
 	  zip_safe=False,
 	  download_url='https://github.com/Hananel-Hazan/bindsnet/archive/%s.tar.gz' % version,
 	  install_requires=['numpy>=1.14.2',
