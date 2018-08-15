d4988a33911c7991409cf9f76405e586b11dc18c
sharath <sharathramku@umass.edu>
2018-08-15 15:36:21 -0400

    tests

--- test/encoders/test_encoders.py
+++ test/encoders/test_encoders.py
@@ -6,5 +6,27 @@ class TestEncoders:
     Tests encoders.
     """
 
-    def test_geospatial_encoder(self):
-        pass
+    def test_numenta_encoder(self):
+        csvfile = './test.csv'
+        encodingfile = './encoding.p'
+
+        def test1():
+            enc = NumentaEncoder(csvfile, save=True, encodingfile=encodingfile)
+            val = enc.get_encoding()
+            assert os.path.exists(encodingfile)
+            os.remove(encodingfile)
+            assert val is not None
+
+        def test2():
+            enc = NumentaEncoder(csvfile, save=False, encodingfile=encodingfile)
+            val = enc.get_encoding()
+            assert val is not None
+            assert not os.path.exists(encodingfile)
+
+        def test3():
+            enc1 = NumentaEncoder(csvfile, save=False, encodingfile=encodingfile)
+            enc2 = NumentaEncoder(csvfile, save=False, encodingfile=encodingfile)
+            v1, v2 = enc1.get_encoding(), enc2.get_encoding()
+            assert torch.all(torch.eq(v1, v2))
+
+        test1(), test2(), test3()
