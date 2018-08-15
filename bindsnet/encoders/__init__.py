b5d13fcfa4dc46e6ca51043dbaa9615665745bdf
sharath <sharathramku@umass.edu>
2018-08-15 12:49:54 -0400

    docs, pyproj for numenta encoding

--- bindsnet/encoders/__init__.py
+++ bindsnet/encoders/__init__.py
@@ -1,12 +1,26 @@
 import hashlib
 import csv
 import os
+from abc import abstractmethod, ABC
+
 import torch
 import pickle
 
 
-class AbstractEncoder:
+class AbstractEncoder(ABC):
+    # language=rst
+    """
+    Abstract base class for Encoder.
+    """
+
     def __init__(self, csvfile: str, save=False, file='./encodings/encoding.p') -> None:
+        # language=rst
+        """
+        Abstract constructor for the Encoder class.
+        :param csvfile: File to encode
+        :param save: Whether to save the encoding to file.
+        :param download: Where to save encoding to. (./encodings/encoding.p) by default
+        """
         self.csvfile = csvfile
         self.save = save
         self.enc = {}
@@ -16,13 +30,31 @@ class AbstractEncoder:
                 os.makedirs(os.path.dirname(file), exist_ok=True)
             self._check_file()
 
+    @abstractmethod
+    def _encode(self) -> torch.Tensor:
+        # language=rst
+        """
+        Abstract method for defining how encoding is done from the csv file.
+         :return: Generated encoding
+        """
+        pass
+
     def _gen_hash(self) -> str:
+        # language=rst
+        """
+        Generates an md5 hash for the csv file.
+         :return: md5 hash for the csv file
+        """
         with open(self.csvfile, 'r') as f:
             lines = f.readlines()
         m = hashlib.md5(''.join(lines).encode('utf-8'))
         return m.hexdigest()
 
-    def _check_file(self):
+    def _check_file(self) -> None:
+        # language=rst
+        """
+        Compares the csv file and the save encoding to see if a new encoding needs to be generated.
+        """
         self.enc['verify'] = self._gen_hash()
         try:
             with open(self.file, 'rb') as f:
@@ -34,14 +66,20 @@ class AbstractEncoder:
         if 'verify' in self.enc and self.enc['verify'] == temp['verify']:
             self.enc = temp
 
-    def _save(self):
+    def _save(self) -> None:
+        # language=rst
+        """
+        Creates/Overwrites existing encoding file
+        """
         with open(self.file, 'wb') as f:
             pickle.dump(self.enc, f)
 
-    def _encode(self) -> torch.Tensor:
-        pass
-
     def get_encoding(self) -> torch.tensor:
+        # language=rst
+        """
+        Returns encoding for the csv file
+        :return: Encoding for specified csv file
+        """
         if 'encoding' in self.enc and self.enc['encoding'] is not None:
             return self.enc['encoding']
         self.enc['encoding'] = self._encode()
