13b3ce4dad789f331547698be327e3409900c06f
Hananel Hazan <hananel@hazan.org.il>
2018-09-05 14:04:49 -0400

    Revert Change the input neuron to support real value

--- bindsnet/network/nodes.py
+++ bindsnet/network/nodes.py
@@ -80,7 +80,7 @@ class Input(Nodes):
     """
 
     def __init__(self, n: Optional[int] = None, shape: Optional[Iterable[int]] = None,
-                 traces: bool = False, trace_tc: float = 5e-2, real_value_input: bool = False) -> None:
+                 traces: bool = False, trace_tc: float = 5e-2) -> None:
         # language=rst
         """
         Instantiates a layer of input neurons.
@@ -91,7 +91,6 @@ class Input(Nodes):
         :param trace_tc: Time constant of spike trace decay.
         """
         super().__init__(n, shape, traces, trace_tc)
-        self.real_value_input = real_value_input;
 
     def step(self, inpts: torch.Tensor, dt: float) -> None:
         # language=rst
@@ -103,10 +102,7 @@ class Input(Nodes):
         """
         # Set spike occurrences to input values.
 
-        if (self.real_value_input):
-            self.s = inpts
-        else:
-            self.s = inpts.byte()
+        self.s = inpts.byte()
 
         super().step(inpts, dt)
 
