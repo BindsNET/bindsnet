45741de33a85fe61fc40ff26de79439badd60732
djsaunde <djsaunde@umass.edu>
2018-09-20 10:52:36 -0400

    More .travis.yml tweaks.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     download_url='https://github.com/Hananel-Hazan/bindsnet/archive/%s.tar.gz' % version,
     install_requires=[
         'numpy>=1.14.2', 'torch>=0.4.1', 'tqdm>=4.19.9', 'matplotlib>=2.1.0', 'gym>=0.10.4', 'scikit_image>=0.13.1',
-        'scikit_learn>=0.19.1', 'opencv-python>=3.4.0.12', 'sphinx_rtd_theme>=0.4.1e', 'pytest>=3.4.0', 'scipy>=1.1.0',
+        'scikit_learn>=0.19.1', 'opencv-python>=3.4.0.12', 'pytest>=3.4.0', 'scipy>=1.1.0',
         'cython>=0.28.5', 'pandas>=0.23.4', 'pyproj<=1.9.5.2'
     ],
     dependency_links=[
