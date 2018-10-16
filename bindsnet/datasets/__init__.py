37773b030c043a2a2663e2ef02edd4e8ad9d1d1b
Dan Saunders <djsaunde@umass.edu>
2018-10-15 23:32:59 -0400

    Fixing random shuffling of SpokenMNIST dataset.

--- bindsnet/datasets/__init__.py
+++ bindsnet/datasets/__init__.py
@@ -529,7 +529,7 @@ class SpokenMNIST(Dataset):
 
         if self.shuffle:
             perm = np.random.permutation(np.arange(labels.shape[0]))
-            audio, labels = audio[perm], labels[perm]
+            audio, labels = [torch.Tensor(audio[_]) for _ in perm], labels[perm]
 
         return audio, torch.Tensor(labels)
 
@@ -618,19 +618,23 @@ class SpokenMNIST(Dataset):
             # Popular settings are 25 ms for the frame size and a 10 ms stride (15 ms overlap)
             frame_size = 0.025
             frame_stride = 0.01
-            frame_length, frame_step = frame_size * sample_rate, frame_stride * sample_rate  # Convert from seconds to samples
+
+            # Convert from seconds to samples
+            frame_length, frame_step = frame_size * sample_rate, frame_stride * sample_rate
             signal_length = len(emphasized_signal)
             frame_length = int(round(frame_length))
             frame_step = int(round(frame_step))
-            num_frames = int(np.ceil(
-                float(np.abs(signal_length - frame_length)) / frame_step))  # Make sure that we have at least 1 frame
+
+            # Make sure that we have at least 1 frame
+            num_frames = int(np.ceil(float(np.abs(signal_length - frame_length)) / frame_step))
 
             pad_signal_length = num_frames * frame_step + frame_length
             z = np.zeros((pad_signal_length - signal_length))
             pad_signal = np.append(emphasized_signal, z)  # Pad signal
 
             indices = np.tile(np.arange(0, frame_length), (num_frames, 1)) + np.tile(
-                np.arange(0, num_frames * frame_step, frame_step), (frame_length, 1)).T
+                np.arange(0, num_frames * frame_step, frame_step), (frame_length, 1)
+            ).T
             frames = pad_signal[indices.astype(np.int32, copy=False)]
 
             # Hamming Window
