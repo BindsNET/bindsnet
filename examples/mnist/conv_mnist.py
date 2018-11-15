e13de78a4172102a4786945610bfc1883cf0848f
Dan Saunders <djsaunde@umass.edu>
2018-11-15 10:50:28 -0500

    Adding dt argument to encoding functions; removing numpy and replacing with torch implementations of encoding functions.

--- examples/mnist/conv_mnist.py
+++ examples/mnist/conv_mnist.py
@@ -120,7 +120,7 @@ images, labels = MNIST(path=os.path.join('..', '..', 'data', 'MNIST'), download=
 images *= intensity
 
 # Lazily encode data as Poisson spike trains.
-data_loader = poisson_loader(data=images, time=time)
+data_loader = poisson_loader(data=images, time=time, dt=dt)
 
 spikes = {}
 for layer in set(network.layers):
