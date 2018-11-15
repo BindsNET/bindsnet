99e525c4d721d390f5efa311da536e11b5d1f88c
Dan Saunders <djsaunde@umass.edu>
2018-11-15 13:16:41 -0500

    Adding un-clamping in network main loop (neurons clamped to not fire).

--- examples/mnist/supervised_mnist.py
+++ examples/mnist/supervised_mnist.py
@@ -160,10 +160,12 @@ for i in range(n_train):
             voltage_ims, voltage_axes = plot_voltages(voltages)
 
         else:
-            inpt_axes, inpt_ims = plot_input(images[i].view(28, 28), inpt, label=labels[i], axes=inpt_axes,
-                                             ims=inpt_ims)
-            spike_ims, spike_axes = plot_spikes({layer: spikes[layer].get('s') for layer in spikes},
-                                                ims=spike_ims, axes=spike_axes)
+            inpt_axes, inpt_ims = plot_input(
+                images[i].view(28, 28), inpt, label=labels[i], axes=inpt_axes, ims=inpt_ims
+            )
+            spike_ims, spike_axes = plot_spikes(
+                {layer: spikes[layer].get('s') for layer in spikes}, ims=spike_ims, axes=spike_axes
+            )
             weights_im = plot_weights(square_weights, im=weights_im)
             assigns_im = plot_assignments(square_assignments, im=assigns_im)
             perf_ax = plot_performance(accuracy, ax=perf_ax)
