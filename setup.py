c4dbc02df639167f925797f09f6792565a41d497
sharath <sharathramkumar@gmail.com>
2018-11-16 15:48:31 -0500

    update setup and requirements to remove pyproj

--- setup.py
+++ setup.py
@@ -21,9 +21,6 @@ setup(
     install_requires=[
         'numpy>=1.14.2', 'torch>=0.4.1', 'tqdm>=4.19.9', 'matplotlib>=2.1.0', 'gym>=0.10.4', 'scikit_image>=0.13.1',
         'scikit_learn>=0.19.1', 'opencv-python>=3.4.0.12', 'pytest>=3.4.0', 'scipy>=1.1.0',
-        'cython>=0.28.5', 'pandas>=0.23.4', 'pyproj<=1.9.5.2'
-    ],
-    dependency_links=[
-        'https://github.com/jswhit/pyproj/archive/429a4fe6fa404ba1bc1c0a88bee68c1a30a9b6f9.zip#egg=pyproj-1.9.5.2'
+        'cython>=0.28.5', 'pandas>=0.23.4'
     ]
 )
