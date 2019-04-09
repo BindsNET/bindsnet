6e29513d260ebf21ae5db1a6eb27196824cabaad
Dan Saunders <djsaunde@umass.edu>
2019-04-09 11:09:32 -0400

    Fixing doc-string example code.

--- bindsnet/network/__init__.py
+++ bindsnet/network/__init__.py
@@ -30,7 +30,6 @@ def load(file_name: str, map_location: str = 'cpu', learning: bool = None) -> 'N
 class Network:
     # language=rst
     """
-
     Most important object of the :code:`bindsnet` package. Responsible for the simulation and interaction of nodes and
     connections.
 
@@ -62,7 +61,7 @@ class Network:
         network.add_monitor(monitor=M2, name='Y')
 
         # Create Poisson-distributed spike train inputs.
-        data = 15 * torch.rand(1, 100)  # Generate random Poisson rates for 100 input neurons.
+        data = 15 * torch.rand(100)  # Generate random Poisson rates for 100 input neurons.
         train = encoding.poisson(datum=data, time=5000)  # Encode input as 5000ms Poisson spike trains.
 
         # Simulate network on generated spike trains.
@@ -172,8 +171,7 @@ class Network:
     def clone(self) -> None:
         # language=rst
         """
-        Returning a clone network object
-
+        Returning a cloned network object.
         """
         virtualFile = tempfile.SpooledTemporaryFile()
         torch.save(self, virtualFile)
