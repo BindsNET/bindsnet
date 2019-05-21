16b998e8a8f608642ed0e564613e53a11f3159c9
k-chaney <chaneyk@seas.upenn.edu>
2019-05-21 13:10:36 -0400

    Only removed redundant imports

--- bindsnet/pipeline/__init__.py
+++ bindsnet/pipeline/__init__.py
@@ -2,9 +2,6 @@ from .environment_pipeline import EnvironmentPipeline
 from .base_pipeline import BasePipeline
 from .dataloader_pipeline import DataLoaderPipeline, TorchVisionDatasetPipeline
 
-from . import pipeline_analysis
-from . import action
-
 __all__ = ['BasePipeline', 'EnvironmentPipeline',
            'DataLoaderPipeline', 'TorchVisionDatasetPipeline',
            'pipeline_analysis', 'action']
