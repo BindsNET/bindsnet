9ca31de6f68d0e5e6c2cf144a8dd6453093d649c
k-chaney <chaneyk@seas.upenn.edu>
2019-06-06 15:57:14 -0400

    Add comments to all functions

--- bindsnet/analysis/pipeline_analysis.py
+++ bindsnet/analysis/pipeline_analysis.py
@@ -21,6 +21,7 @@ class PipelineAnalyzer(ABC):
 
     @abstractmethod
     def finalize_step(self) -> None:
+        # language=rst
         """
         Flush the output from the current step
         """
@@ -28,23 +29,85 @@ class PipelineAnalyzer(ABC):
 
     @abstractmethod
     def plot_obs(self, obs, tag="obs", step: int = None) -> None:
+        # language=rst
+        """
+        Pulls the observation off of torch and sets up for matplotlib
+        plotting.
+
+        :param obs: A 2D array of floats depicting an input image.
+        :param tag: A unique tag to associate the data with.
+        :param step: The step of the pipeline.
+        """
         pass
 
     @abstractmethod
-    def plot_reward(self, reward_list, reward_window: int = None, tag="reward", step: int = None) -> None:
+    def plot_reward(
+        self, reward_list, reward_window: int = None, tag="reward", step: int = None
+    ) -> None:
+        # language=rst
+        """
+        Plot the accumulated reward for each episode.
+
+        :param list reward_list: The list of recent rewards to be plotted
+        :param int reward_window: The length of the window to compute a
+                                  moving average over
+        :param tag: A unique tag to associate the data with.
+        :param step: The step of the pipeline.
+        """
         pass
 
     @abstractmethod
-    def plot_spikes(self, spike_record: Dict[str, torch.Tensor], tag="spike", step: int = None):
+    def plot_spikes(
+        self, spike_record: Dict[str, torch.Tensor], tag="spike", step: int = None
+    ):
+        # language=rst
+        """
+        Plots all spike records inside of spike_record. Keeps unique
+        plots for all unique tags that are given.
+
+        :param dict spike_record: Dictionary of spikes to be rasterized
+        :param tag: A unique tag to associate the data with.
+        :param step: The step of the pipeline.
+        """
         pass
 
     @abstractmethod
-    def plot_voltage(self, voltage_record, threshold_value, tag="voltage", step: int = None):
+    def plot_voltage(
+        self, voltage_record, threshold_value, tag="voltage", step: int = None
+    ):
+        # language=rst
+        """
+        Plots all voltage records and given thresholds. Keeps unique
+        plots for all unique tags that are given.
+
+        :param dict voltage_record: Dictionary of voltages for neurons
+        inside of networks organized by the layer they correspond to
+        neurons
+        :param dict threshold_value: Dictionary of threshold values for
+        :param tag: A unique tag to associate the data with.
+        :param step: The step of the pipeline.
+        """
         pass
 
     @abstractmethod
-    def plot_conv2d_weights(self, weights: torch.Tensor, wmin:
-            float=0.0, wmax: float=1.0, tag: str="conv2d", step: int = 0):
+    def plot_conv2d_weights(
+        self,
+        weights: torch.Tensor,
+        wmin: float = 0.0,
+        wmax: float = 1.0,
+        tag: str = "conv2d",
+        step: int = 0,
+    ) -> None:
+        # language=rst
+        """
+        Plot a connection weight matrix of a Conv2dConnection.
+
+        :param weights: Weight matrix of Conv2dConnection object.
+        :param wmin: Minimum allowed weight value.
+        :param wmax: Maximum allowed weight value.
+        :param tag: A unique tag to associate the data with.
+        :param step: The step of the pipeline.
+        """
         pass
 
 
@@ -63,12 +126,16 @@ class MatplotlibAnalyzer(PipelineAnalyzer):
         plt.ion()
         self.plots = {}
 
-    def plot_obs(self, obs, tag="obs", step: int = None) -> None:
+    def plot_obs(self, obs: torch.Tensor, tag: str = "obs", step: int = None) -> None:
+        # language=rst
         """
         Pulls the observation off of torch and sets up for matplotlib
         plotting.
-        """
 
+        :param obs: A 2D array of floats depicting an input image.
+        :param tag: A unique tag to associate the data with.
+        :param step: The step of the pipeline.
+        """
         obs = obs.detach().cpu().numpy()
         obs = np.transpose(obs, (1, 2, 0)).squeeze()
 
@@ -88,7 +155,9 @@ class MatplotlibAnalyzer(PipelineAnalyzer):
         else:
             obs_im.set_data(obs)
 
-    def plot_reward(self, reward_list, reward_window: int = None, tag="reward", step: int = None) -> None:
+    def plot_reward(
+        self, reward_list, reward_window: int = None, tag="reward", step: int = None
+    ) -> None:
         # language=rst
         """
         Plot the accumulated reward for each episode.
@@ -96,6 +165,8 @@ class MatplotlibAnalyzer(PipelineAnalyzer):
         :param list reward_list: The list of recent rewards to be plotted
         :param int reward_window: The length of the window to compute a
                                   moving average over
+        :param tag: A unique tag to associate the data with.
+        :param step: The step of the pipeline.
         """
         if tag in self.plots:
             reward_ax, reward_im = self.plots[tag]
@@ -130,29 +201,38 @@ class MatplotlibAnalyzer(PipelineAnalyzer):
             reward_ax.relim()
             reward_ax.autoscale_view()
 
-    def plot_spikes(self, spike_record: Dict[str, torch.Tensor], tag="spike", step: int = None):
+    def plot_spikes(
+        self, spike_record: Dict[str, torch.Tensor], tag="spike", step: int = None
+    ) -> None:
+        # language=rst
         """
         Plots all spike records inside of spike_record. Keeps unique
         plots for all unique tags that are given.
 
         :param dict spike_record: Dictionary of spikes to be rasterized
+        :param tag: A unique tag to associate the data with.
+        :param step: The step of the pipeline.
         """
-
         if tag not in self.plots:
             self.plots[tag] = plot_spikes(spike_record)
         else:
             s_im, s_ax = self.plots[tag]
             self.plots[tag] = plot_spikes(spike_record, ims=s_im, axes=s_ax)
 
-    def plot_voltage(self, voltage_record, threshold_value, tag="voltage", step: int = None):
+    def plot_voltage(
+        self, voltage_record, threshold_value, tag="voltage", step: int = None
+    ) -> None:
+        # language=rst
         """
         Plots all voltage records and given thresholds. Keeps unique
         plots for all unique tags that are given.
 
         :param dict voltage_record: Dictionary of voltages for neurons
         inside of networks organized by the layer they correspond to
-        :param dict threshold_value: Dictionary of threshold values for
         neurons
+        :param dict threshold_value: Dictionary of threshold values for
+        :param tag: A unique tag to associate the data with.
+        :param step: The step of the pipeline.
         """
         if tag not in self.plots:
             self.plots[tag] = plot_voltages(
@@ -168,39 +248,54 @@ class MatplotlibAnalyzer(PipelineAnalyzer):
                 threshold=threshold_value,
             )
 
-    def plot_data(self, spike_record, voltage_record, threshold_value, tag="data", step: int = None):
-        """
-        Convience function that wraps plot_spikes and plot_voltage in a
-        single call.
-
-        :param dict spike_record: Dictionary of spikes to be rasterized
-        :param dict voltage_record: Dictionary of voltages for neurons
-        inside of networks organized by the layer they correspond to
-        :param dict threshold_value: Dictionary of threshold values for
-        neurons
-        """
+    def plot_data(
+        self,
+        spike_record,
+        voltage_record,
+        threshold_value,
+        tag="data",
+        step: int = None,
+    ) -> None:
         # Initialize plots
         self.plot_spikes(spike_record, tag + "_s")
         self.plot_voltage(voltage_record, threshold_value, tag + "_v")
 
-    def plot_conv2d_weights(self, weights: torch.Tensor, wmin:
-            float=0.0, wmax: float=1.0, tag: str="conv2d", step: int = 0):
+    def plot_conv2d_weights(
+        self,
+        weights: torch.Tensor,
+        wmin: float = 0.0,
+        wmax: float = 1.0,
+        tag: str = "conv2d",
+        step: int = 0,
+    ) -> None:
+        # language=rst
+        """
+        Plot a connection weight matrix of a Conv2dConnection.
 
+        :param weights: Weight matrix of Conv2dConnection object.
+        :param wmin: Minimum allowed weight value.
+        :param wmax: Maximum allowed weight value.
+        :param tag: A unique tag to associate the data with.
+        :param step: The step of the pipeline.
+        """
         if tag not in self.plots:
             self.plots[tag] = plot_conv2d_weights(weights, wmin, wmax)
         else:
             im = self.plots[tag]
             plot_conv2d_weights(weights, wmin, wmax, im=im)
 
-
-    def finalize_step(self):
+    def finalize_step(self) -> None:
+        # language=rst
+        """
+        Flush the output from the current step
+        """
         plt.draw()
         plt.pause(1e-8)
         plt.show()
 
 
 class TensorboardAnalyzer(PipelineAnalyzer):
-    def __init__(self, summary_directory='./logs'):
+    def __init__(self, summary_directory="./logs"):
         self.writer = SummaryWriter(summary_directory)
 
     def finalize_step(self) -> None:
@@ -210,35 +305,95 @@ class TensorboardAnalyzer(PipelineAnalyzer):
         pass
 
     def plot_obs(self, obs, tag="obs", step: int = None) -> None:
+        # language=rst
+        """
+        Pulls the observation off of torch and sets up for matplotlib
+        plotting.
+
+        :param obs: A 2D array of floats depicting an input image.
+        :param tag: A unique tag to associate the data with.
+        :param step: The step of the pipeline.
+        """
         obs_grid = make_grid(obs.float(), nrow=4, normalize=True)
         self.writer.add_image(tag, obs_grid, step)
 
-    def plot_reward(self, reward_list, reward_window: int = None,
-            tag="reward", step: int = None) -> None:
+    def plot_reward(
+        self, reward_list, reward_window: int = None, tag="reward", step: int = None
+    ) -> None:
+        # language=rst
+        """
+        Plot the accumulated reward for each episode.
+
+        :param list reward_list: The list of recent rewards to be plotted
+        :param int reward_window: The length of the window to compute a
+                                  moving average over
+        :param tag: A unique tag to associate the data with.
+        :param step: The step of the pipeline.
+        """
         self.writer.add_scalar(tag, reward_list[-1], step)
 
-    def plot_spikes(self, spike_record: Dict[str, torch.Tensor],
-            tag="spike", step: int = None) -> None:
+    def plot_spikes(
+        self, spike_record: Dict[str, torch.Tensor], tag="spike", step: int = None
+    ) -> None:
+        # language=rst
+        """
+        Plots all spike records inside of spike_record. Keeps unique
+        plots for all unique tags that are given.
+
+        :param dict spike_record: Dictionary of spikes to be rasterized
+        :param tag: A unique tag to associate the data with.
+        :param step: The step of the pipeline.
+        """
         for k, spikes in spike_record.items():
             # shuffle spikes into 1x1x#NueronsxT
             spikes = spikes.view(1, 1, -1, spikes.shape[-1]).float()
             spike_grid_img = make_grid(spikes, nrow=1, pad_value=0.5)
 
-            self.writer.add_image(tag+"_"+str(k), spike_grid_img, step)
+            self.writer.add_image(tag + "_" + str(k), spike_grid_img, step)
 
-    def plot_voltage(self, voltage_record: Dict[str, torch.Tensor],
-            threshold_value: Dict[str, float],
-            tag="voltage", step: int = None) -> None:
+    def plot_voltage(
+        self,
+        voltage_record: Dict[str, torch.Tensor],
+        threshold_value: Dict[str, float],
+        tag="voltage",
+        step: int = None,
+    ) -> None:
+        # language=rst
+        """
+        Plots all voltage records and given thresholds. Keeps unique
+        plots for all unique tags that are given.
+
+        :param dict voltage_record: Dictionary of voltages for neurons
+        inside of networks organized by the layer they correspond to
+        neurons
+        :param dict threshold_value: Dictionary of threshold values for
+        :param tag: A unique tag to associate the data with.
+        :param step: The step of the pipeline.
+        """
         for k, v in voltage_record.items():
             # shuffle voltages into 1x1x#NueronsxT
             v = v.view(1, 1, -1, v.shape[-1])
             voltage_grid_img = make_grid(v, nrow=1, pad_value=0)
 
-            self.writer.add_image(tag+"_"+str(k), voltage_grid_img, step)
+            self.writer.add_image(tag + "_" + str(k), voltage_grid_img, step)
 
-    def plot_conv2d_weights(self, weights: torch.Tensor, wmin:
-            float=0.0, wmax: float=1.0, tag: str="conv2d", step: int = 0):
+    def plot_conv2d_weights(
+        self,
+        weights: torch.Tensor,
+        wmin: float = 0.0,
+        wmax: float = 1.0,
+        tag: str = "conv2d",
+        step: int = 0,
+    ) -> None:
+        # language=rst
+        """
+        Plot a connection weight matrix of a Conv2dConnection.
 
-        reshaped = reshape_conv2d_weights(weights, wmin,
-                wmax).unsqueeze(0)
+        :param weights: Weight matrix of Conv2dConnection object.
+        :param wmin: Minimum allowed weight value.
+        :param wmax: Maximum allowed weight value.
+        :param tag: A unique tag to associate the data with.
+        :param step: The step of the pipeline.
+        """
+        reshaped = reshape_conv2d_weights(weights, wmin, wmax).unsqueeze(0)
         self.writer.add_image(tag, reshaped, step)
