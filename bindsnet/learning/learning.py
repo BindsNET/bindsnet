86bd2c90a99ca21fe2598381dd4369c706e55011
Simon Caby <simoncaby28@gmail>
2020-11-16 14:08:47 +0100

    faster PostPre by changing nu*trace and del unused temp tensors

--- bindsnet/learning/learning.py
+++ bindsnet/learning/learning.py
@@ -177,20 +177,19 @@ class PostPre(LearningRule):
         """
         batch_size = self.source.batch_size
 
-        source_s = self.source.s.view(batch_size, -1).unsqueeze(2).float()
-        source_x = self.source.x.view(batch_size, -1).unsqueeze(2)
-        target_s = self.target.s.view(batch_size, -1).unsqueeze(1).float()
-        target_x = self.target.x.view(batch_size, -1).unsqueeze(1)
-
         # Pre-synaptic update.
         if self.nu[0]:
-            update = self.reduction(torch.bmm(source_s, target_x), dim=0)
-            self.connection.w -= self.nu[0] * update
+            source_s = self.source.s.view(batch_size, -1).unsqueeze(2).float()
+            target_x = self.target.x.view(batch_size, -1).unsqueeze(1) * self.nu[0]
+            self.connection.w -= self.reduction(torch.bmm(source_s, target_x), dim=0)
+            del source_s, target_x
 
         # Post-synaptic update.
         if self.nu[1]:
-            update = self.reduction(torch.bmm(source_x, target_s), dim=0)
-            self.connection.w += self.nu[1] * update
+            source_x = self.source.x.view(batch_size, -1).unsqueeze(2)
+            target_s = self.target.s.view(batch_size, -1).unsqueeze(1).float() * self.nu[1]
+            self.connection.w += self.reduction(torch.bmm(source_x, target_s), dim=0)
+            del source_x, target_s
 
         super().update()
 
