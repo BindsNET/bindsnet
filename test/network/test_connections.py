2a7259a0ab78f9a80543ad2fa7bdb43c77e11893
Mahbodnr <mahbodnouri@gmail.com>
2021-07-21 00:49:16 +0430

    add other learning rules and connections

--- test/network/test_connections.py
+++ test/network/test_connections.py
@@ -1,12 +1,19 @@
 import torch
 
 from bindsnet.network import Network
-from bindsnet.network.nodes import LIFNodes
+from bindsnet.network.nodes import Input, LIFNodes, SRM0Nodes
 
 from bindsnet.network.topology import *
 
-from bindsnet.learning import Hebbian, PostPre
-
+from bindsnet.learning import (
+    Hebbian,
+    PostPre,
+    WeightDependentPostPre,
+    MSTDP,
+    MSTDPET,
+    Rmax,
+    NoOp,
+)
 class TestConnection:
     """
     Tests all stable groups of neurons / nodes.
@@ -65,6 +72,7 @@ class TestConnection:
 
 
     def test_weights(self, conn_type, shape_a, shape_b, shape_w, *args, **kwargs):
+        print("Testing:", conn_type)
         time = 100
         weights = [None, torch.Tensor(*shape_w)]
         wmins = [-np.inf, 0, torch.zeros(*shape_w)]
@@ -72,28 +80,73 @@ class TestConnection:
         for w in weights:
             for wmin in wmins:
                 for wmax in wmaxes:
-                    print(f"Testing {conn_type} with: w={w}, wmin={wmin}, wmax={wmax}")
-                    l_a = LIFNodes(shape= shape_a, traces = True)
-                    l_b = LIFNodes(shape= shape_b, traces = True)
-                    conn = conn_type(l_a, l_b, w = w, wmin =wmin, wmax =wmax,
-                                     *args, **kwargs)
+                    print(
+                        f"- w: {type(w).__name__}, "
+                        f"wmin: {type(wmax).__name__}, wmax: {type(wmax).__name__}"
+                    )
+                    if kwargs.get('update_rule') == Rmax:
+                        l_a = SRM0Nodes(shape= shape_a, traces = True, traces_additive = True)
+                        l_b = SRM0Nodes(shape= shape_b, traces = True, traces_additive = True)
+                    else:
+                        l_a = LIFNodes(shape= shape_a, traces = True, traces_additive = True)
+                        l_b = LIFNodes(shape= shape_b, traces = True, traces_additive = True)
+
 
                     network = Network(dt=1.0)
+                    network.add_layer(
+                        Input(n=100, traces = True, traces_additive = True),
+                        name="input"
+                    )
                     network.add_layer(l_a, name="a")
                     network.add_layer(l_b, name="b")
-                    network.add_connection(conn, source= 'a', target= 'b')
+
+                    network.add_connection(
+                        conn_type(l_a, l_b, w = w, wmin =wmin, wmax =wmax,
+                                 *args, **kwargs), source= 'a', target= 'b')
+                    network.add_connection(
+                        Connection(
+                            source=network.layers["input"],
+                            target=network.layers["a"],
+                            **kwargs,
+                        ),
+                        source="input",
+                        target="a",
+                    )
                     network.run(
-                        inputs={}, 
-                        time=time
+                        inputs={"input": torch.bernoulli(torch.rand(time, 100)).byte()}, 
+                        time=time,
+                        reward = 1,
                     )
 
 if __name__ == "__main__":
     tester = TestConnection()
 
-    # tester.test_transfer()
-    tester.test_weights(Connection, [100], [50], (100,50),
-                        nu = 1E-2, update_rule=Hebbian)
-    tester.test_weights(Conv2dConnection, [1,28,28], [1,26,26], (1,1,3,3), 3, norm = 2,
-                        nu = 1E-2, update_rule=Hebbian)
-    tester.test_weights(LocalConnection, [1,28,28], [1,26,26], (784, 676), 3, 1, 1,
-                        nu = 1E-2, update_rule=Hebbian)
+    tester.test_transfer()
+
+    # Connections with learning ability
+    conn_types = [Connection, Conv2dConnection, LocalConnection]
+    args =[
+        [[100], [50], (100,50)], 
+        [[1,28,28], [1,26,26], (1,1,3,3), 3,],
+        [[1,28,28], [1,26,26], (784, 676), 3, 1, 1,]
+    ]
+    for update_rule in (
+        Hebbian,
+        PostPre,
+        # WeightDependentPostPre,
+        MSTDP,
+        MSTDPET,
+        # Rmax,
+    ):
+        print("Learning Rule:", update_rule)
+        for conn_type, arg in zip(conn_types, args):
+            tester.test_weights(conn_type, nu = 1E-2, update_rule=update_rule, *arg)
+    
+    # Other connections
+    conn_types = [MeanFieldConnection, MaxPool2dConnection]
+    args =[
+        [[1,28,28], [1,26,26], (1,1,3,3), 3, 1], 
+        [[100], [50], [1]],
+    ]
+    for conn_type, arg in zip(conn_types, args):
+        tester.test_weights(conn_type, decay = 1, update_rule = NoOp, *arg)
