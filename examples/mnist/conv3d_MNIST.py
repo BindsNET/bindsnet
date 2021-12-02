8d3478d45c5942eae0896a33896b316f6d5f300b
Hafez Ghaemi <ghaemi.hafez@gmail.com>
2021-12-02 22:37:09 +0100

    Relocate conv1d and conv3d MNIST examples

---
+++ examples/mnist/conv3d_MNIST.py
@@ -0,0 +1,186 @@
+### Toy example to test Conv3dConnection (the dataset used is MNIST but with a dimension replicated 
+### for each image (each sample has size (28, 28, 28))
+
+import argparse
+import os
+from time import time as t
+
+import torch
+from torchvision import transforms
+from tqdm import tqdm
+
+from bindsnet.datasets import MNIST
+from bindsnet.encoding import PoissonEncoder
+from bindsnet.learning import PostPre
+from bindsnet.network import Network
+from bindsnet.network.monitors import Monitor
+from bindsnet.network.nodes import DiehlAndCookNodes, Input
+from bindsnet.network.topology import Connection, Conv3dConnection
+
+print()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--seed", type=int, default=0)
+parser.add_argument("--n_epochs", type=int, default=1)
+parser.add_argument("--n_test", type=int, default=10000)
+parser.add_argument("--n_train", type=int, default=60000)
+parser.add_argument("--batch_size", type=int, default=1)
+parser.add_argument("--kernel_size", type=int, default=16)
+parser.add_argument("--stride", type=int, default=4)
+parser.add_argument("--n_filters", type=int, default=25)
+parser.add_argument("--padding", type=int, default=0)
+parser.add_argument("--time", type=int, default=50)
+parser.add_argument("--dt", type=int, default=1.0)
+parser.add_argument("--intensity", type=float, default=128.0)
+parser.add_argument("--progress_interval", type=int, default=10)
+parser.add_argument("--update_interval", type=int, default=250)
+parser.add_argument("--train", dest="train", action="store_true")
+parser.add_argument("--test", dest="train", action="store_false")
+parser.add_argument("--plot", dest="plot", action="store_true")
+parser.add_argument("--gpu", dest="gpu", action="store_true")
+parser.set_defaults(plot=True, gpu=True, train=True)
+
+args = parser.parse_args()
+
+seed = args.seed
+n_epochs = args.n_epochs
+n_test = args.n_test
+n_train = args.n_train
+batch_size = args.batch_size
+kernel_size = args.kernel_size
+stride = args.stride
+n_filters = args.n_filters
+padding = args.padding
+time = args.time
+dt = args.dt
+intensity = args.intensity
+progress_interval = args.progress_interval
+update_interval = args.update_interval
+train = args.train
+plot = args.plot
+gpu = args.gpu
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if gpu and torch.cuda.is_available():
+    torch.cuda.manual_seed_all(seed)
+else:
+    torch.manual_seed(seed)
+    device = "cpu"
+    if gpu:
+        gpu = False
+
+torch.set_num_threads(os.cpu_count() - 1)
+print("Running on Device = ", device)
+
+if not train:
+    update_interval = n_test
+
+conv_size = int((28 - kernel_size + 2 * padding) / stride) + 1
+per_class = int((n_filters * conv_size) / 10)
+
+# Build network.
+network = Network()
+input_layer = Input(n=28*28*28, shape=(1, 28, 28, 28), traces=True)
+
+conv_layer = DiehlAndCookNodes(
+    n=n_filters * conv_size * conv_size * conv_size,
+    shape=(n_filters, conv_size, conv_size, conv_size),
+    traces=True,
+)
+
+conv_conn = Conv3dConnection(
+    input_layer,
+    conv_layer,
+    kernel_size=kernel_size,
+    stride=stride,
+    update_rule=PostPre,
+    norm=0.4 * kernel_size ** 3,
+    nu=[1e-4, 1e-2],
+    wmax=1.0,
+)
+
+w = torch.zeros(n_filters, conv_size, conv_size, conv_size, n_filters, conv_size, conv_size, conv_size)
+for fltr1 in range(n_filters):
+    for fltr2 in range(n_filters):
+        if fltr1 != fltr2:
+            for i in range(conv_size):
+                for j in range(conv_size):
+                    for k in range(conv_size):
+                        w[fltr1, i, j, k, fltr2, i, j, k] = -100.0
+
+w = w.view(n_filters * conv_size * conv_size * conv_size, n_filters * conv_size * conv_size * conv_size)
+recurrent_conn = Connection(conv_layer, conv_layer, w=w)
+
+network.add_layer(input_layer, name="X")
+network.add_layer(conv_layer, name="Y")
+network.add_connection(conv_conn, source="X", target="Y")
+network.add_connection(recurrent_conn, source="Y", target="Y")
+
+# Voltage recording for excitatory and inhibitory layers.
+voltage_monitor = Monitor(network.layers["Y"], ["v"], time=time)
+network.add_monitor(voltage_monitor, name="output_voltage")
+
+if gpu:
+    network.to("cuda")
+
+
+
+# Load MNIST data.
+train_dataset = MNIST(
+    PoissonEncoder(time=time, dt=dt),
+    None,
+    "../../data/MNIST",
+    download=True,
+    train=True,
+    transform=transforms.Compose(
+        [transforms.ToTensor(), transforms.Lambda(lambda x: x * intensity)]
+    ),
+)
+
+spikes = {}
+for layer in set(network.layers):
+    spikes[layer] = Monitor(network.layers[layer], state_vars=["s"], time=time)
+    network.add_monitor(spikes[layer], name="%s_spikes" % layer)
+
+voltages = {}
+for layer in set(network.layers) - {"X"}:
+    voltages[layer] = Monitor(network.layers[layer], state_vars=["v"], time=time)
+    network.add_monitor(voltages[layer], name="%s_voltages" % layer)
+
+# Train the network.
+print("Begin training.\n")
+start = t()
+
+inpt_axes = None
+inpt_ims = None
+spike_ims = None
+spike_axes = None
+weights1_im = None
+voltage_ims = None
+voltage_axes = None
+
+for epoch in range(n_epochs):
+    if epoch % progress_interval == 0:
+        print("Progress: %d / %d (%.4f seconds)" % (epoch, n_epochs, t() - start))
+        start = t()
+
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=gpu
+    )
+
+    for step, batch in enumerate(tqdm(train_dataloader)):
+        # Get next input sample (expanded to have shape (time, batch_size, 1, 28, 28))
+        if step > n_train:
+            break
+        inputs = {"X": batch["encoded_image"].view(time, batch_size, 1, 28, 28).unsqueeze(3).repeat(1,1,1,28,1,1).float()}
+        if gpu:
+            inputs = {k: v.cuda() for k, v in inputs.items()}
+        label = batch["label"]
+
+        # Run the network on the input.
+        network.run(inputs=inputs, time=time, input_time_dim=1)
+
+        network.reset_state_variables()  # Reset state variables.
+
+print("Progress: %d / %d (%.4f seconds)\n" % (n_epochs, n_epochs, t() - start))
+print("Training complete.\n")
