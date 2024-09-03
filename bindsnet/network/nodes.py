8a1a1d0d969330bd488c6d010a24803d54f2583c
christopher-earl <cearl@umass.edu>
2024-09-02 22:51:02 -0400

    reset state vars for Input nodes

--- bindsnet/network/nodes.py
+++ bindsnet/network/nodes.py
@@ -225,6 +225,8 @@ class Input(Nodes, AbstractInput):
         """
         Resets relevant state variables.
         """
+        self.s.zero_()
+        self.v.zero_()
         super().reset_state_variables()
 
 
