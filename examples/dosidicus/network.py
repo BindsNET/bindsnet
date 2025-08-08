3bc3b006cf14e755ffdb28d63cbc835bbb981a47
nikos <nikdranik2@gmail.com>
2025-08-07 20:45:55 -0400

    Draft

---
+++ examples/dosidicus/network.py
@@ -0,0 +1,38 @@
+import torch
+
+from bindsnet.network import Network
+from bindsnet.network.nodes import LIFNodes
+from bindsnet.network.topology import MulticompartmentConnection
+from bindsnet.network.topology_features import Weight, Bias, Mask
+from bindsnet.learning.MCC_learning import PostPre
+
+network = Network(dt=1.0)
+source_layer = LIFNodes(n=5, traces=True)
+target_layer = LIFNodes(n=5, traces=True)
+
+
+network.add_layer(source_layer, name="input")
+network.add_layer(target_layer, name="output")
+
+weight = Weight(
+    name='weight_feature',
+    value=torch.rand(5, 5),
+    learning_rule=PostPre,
+    nu=(1e-4, 1e-2)
+)
+bias = Bias(name='bias_feature', value=torch.rand(5, 5))
+
+mask = torch.tril(torch.ones((5, 5)), diagonal=-1).bool()
+
+connection = MulticompartmentConnection(
+    source=source_layer,
+    target=target_layer,
+    pipeline=[weight, Mask(name='mask', value=mask), bias],
+    device='cpu'
+)
+network.add_connection(connection, source="input", target="output")
+print(connection.pipeline[0].value)
+network.run(
+    inputs={"input": torch.bernoulli(torch.rand(250, 5)).byte()}, time=250
+)
+print(connection.pipeline[0].value)
